3f6cbe2789126f4e1a62ba9dd6f44450d5995117
Prabesh <poudelprabesh100@gmail.com>
2026-06-18 20:36:42 +0545

    used pointers

--- classSession.cpp
+++ classSession.cpp
@@ -1,8 +1,9 @@
 #include "timeslot.hpp"
-#include "classSession.hpp" // Fixed: Standardized filename casing
+#include "classSession.hpp" 
 
-// Fixed: Reordered initializer list to perfectly match the class declaration order (room before batch)
-ClassSession::ClassSession(TimeSlot timeslot, Instructor teacherid, Course subjectid, Room roomid, StudentBatch batchid)
+
+// The reordered initializer list is perfect!
+ClassSession::ClassSession(TimeSlot timeslot, Instructor* teacherid, Course* subjectid, Room* roomid, StudentBatch* batchid)
     : m_timeslot(timeslot), m_teacherid(teacherid), m_subjectid(subjectid), m_roomid(roomid), m_batchid(batchid)
 {
 }
@@ -11,18 +12,18 @@ TimeSlot ClassSession::getTimeSlot() const {
     return m_timeslot;
 }
 
-Instructor ClassSession::getTeacherId() const {
+Instructor* ClassSession::getTeacherId() const {
     return m_teacherid;
 }
 
-Course ClassSession::getSubjectId() const {
+Course* ClassSession::getSubjectId() const {
     return m_subjectid;
 }
 
-Room ClassSession::getRoomId() const {
+Room* ClassSession::getRoomId() const {
     return m_roomid;
 }
 
-StudentBatch ClassSession::getBatchId() const {
+StudentBatch* ClassSession::getBatchId() const {
     return m_batchid;
 }
--- classSession.hpp
+++ classSession.hpp
@@ -11,19 +11,19 @@
 class ClassSession {
 private:
     TimeSlot m_timeslot;
-    Instructor m_teacherid;
-    Course m_subjectid;
-    Room m_roomid;
-    StudentBatch m_batchid;
+    Instructor* m_teacherid;
+    Course* m_subjectid;
+    Room* m_roomid;
+    StudentBatch* m_batchid;
 
 public:
-    ClassSession(TimeSlot timeslot, Instructor teacherid, Course subjectid, Room roomid, StudentBatch batchid);
+    ClassSession(TimeSlot timeslot, Instructor* teacherid, Course* subjectid, Room* roomid, StudentBatch* batchid);
     
     TimeSlot getTimeSlot() const;
-    Instructor getTeacherId() const;
-    Course getSubjectId() const;
-    Room getRoomId() const;
-    StudentBatch getBatchId() const;
+    Instructor* getTeacherId() const;
+    Course* getSubjectId() const;
+    Room* getRoomId() const;
+    StudentBatch* getBatchId() const;
 };
 
 #endif
