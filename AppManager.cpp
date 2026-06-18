2ebccad264667a1af8cf1c88870537b0fc55ab62
Prabesh <poudelprabesh100@gmail.com>
2026-06-18 22:07:02 +0545

    feat:master-class file 'appmanager' files were added

---
+++ AppManager.cpp
@@ -0,0 +1,56 @@
+#include "AppManager.hpp"
+void AppManager::addInstructor(const Instructor& instructor){
+    m_masterInstructors.push_back(instructor);
+}
+void AppManager::addRoom(const Room& room){
+    m_masterRooms.push_back(room);
+}
+void AppManager::addCourse(const Course& course){
+    m_masterCourses.push_back(course);
+}
+void AppManager::addBatch(const StudentBatch& batch ){
+    m_masterBatches.push_back(batch);
+
+}
+void AppManager::addClassSession(const ClassSession& session){
+    m_timetable.push_bacK(session);
+
+}
+Instructor* AppManager::findInstructorByName(const std::string& name){
+ for(size_t i=0;i<m_masterInstructors.size(); ++i){
+    if(m_masterInstructors[i].getName()==name){
+        return &m_masterInstructors[i];
+
+    }
+ }
+ return nullptr;
+}
+Course* AppManager::findCourseByCode(const std::string& code){
+    for(size_t i=0;i<m_masterCourses.size();++i){
+       if(m_masterCourses[i].getCourseCode()==code){
+        return &m_masterCourses[i];
+       }
+    }
+    return nullptr;
+}
+StudentBatch* AppManager::findBatchById(const std::string& id){
+    for(size_t i=0;i<m_masterBatches.size();i++){
+        if(m_masterBatches[i].getBatchId()==id ){
+            return &m_masterBatches[i];
+        }
+    }
+    return nullptr;
+}
+Room* AppManager::findRoomById(const std::string& id){
+    for(size_t i=0;i<m_masterRooms.size();i++){
+        if(m_masterRooms[i].getRoomId()==id){
+            return &m_masterRooms[i];
+
+        }
+    }
+    return nullptr;
+
+}
+const std::vector<ClassSession>& AppManager::getTimetable() const{
+    return m_timetable;
+}
---
+++ AppManager.hpp
@@ -0,0 +1,36 @@
+#ifndef APP_MANAGER_HPP
+#define APP_MAMNAGER_HPP
+#include <vector>
+#include <string>
+#include "Instructor.hpp"
+#include "Course.hpp"
+#include "Room.hpp"
+#include "Student_Batch.hpp"
+#include "classSession.hpp"
+class AppManager{
+    private:
+    std::vector<Instructor> m_masterInstructors;
+    std::vector<Course> m_masterCourses;
+    std::vector<Room> m_masterRooms;
+    std::vector<StudentBatch> m_masterBatches;
+    
+    std::vector<ClassSession> m_timetable; //Final generated Container
+    public:
+    AppManager()= default;
+    //samip ko qt le call garna parxa
+    void addInstructor(const Instructor& instructor);
+    void addRoom(const Room& room);
+    void addCourse(const Course& course);
+    void addBatch(const StudentBatch& batch);
+    void addClassSession(const ClassSession& session);
+
+    Instructor* findInstructorByName(const std::string& name);
+    Course* findCourseByCode(const std::string& code);
+    Room* findRoomById(const std::string& id);
+    StudentBatch* findBatchById(const std::string& id);
+
+    const std::vector<ClassSession>& getTimetable() const;
+
+
+} ;
+#endif
